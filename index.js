fcf11650dd09aea2d8ef64034e2fc7372a51f2d7
jati <jati.devopsppsi@gmail.com>
2020-03-15 10:07:42 +0700

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+var Client = require('ftp');
+
+module.exports = {
+    getPhotoFtp: async (pathPhoto,ftpHost,ftpUsername,ftpPassword) => {
+        return new Promise((resolve, reject) => {
+            data = ''
+            result64 = ''
+            var c = new Client();
+            c.on('ready', function () {
+                c.get(pathPhoto, function (err, stream) {
+                    if (err) {
+                        resolve('error no file');
+                        c.end();
+                        return;
+                    }
+                    stream.once('close', function () { c.end(); });
+                    stream.on("data", function (chunk) {
+                        result64 = new Buffer.from(chunk).toString('base64')
+                        data = data + result64
+                        resolve(data);
+                    });
+
+                });
+            });
+            c.connect({ host: ftpHost, user: ftpUsername, password: ftpPassword })
+        })
+    }
+}
